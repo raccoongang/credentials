01a846ed66d130b2ab0886afdb2c425794fa71da
Kyrylo Kholodenko <59133933+Thposbsmt@users.noreply.github.com>
2023-01-25 16:05:23 +0200

    feat: [OeX_Cred-158] add 'verifiable_credentials' main toggle (#9)
    Co-authored-by: Kyrylo Kholodenko <kyrylo.kholodenko@raccoongang.com>

--- credentials/urls.py
+++ credentials/urls.py
@@ -64,14 +64,18 @@ urlpatterns = oauth2_urlpatterns + [
     re_path(r"^favicon\.ico$", FaviconView.as_view(permanent=True)),
     re_path(r"^mock-toggles$", MockToggleStateView.as_view()),
     re_path(r"^hijack/", include("hijack.urls", namespace="hijack")),
-    re_path(
-        r"^verifiable-credentials/",
-        include(
-            ("credentials.apps.verifiable_credentials.urls", "verifiable_credentials"),
-            namespace="verifiable_credentials",
-        ),
-    ),
 ]
+
+if settings.ENABLE_VERIFIABLE_CREDENTIALS:
+    urlpatterns += [
+        re_path(
+            r"^verifiable-credentials/",
+            include(
+                ("credentials.apps.verifiable_credentials.urls", "verifiable_credentials"),
+                namespace="verifiable_credentials",
+            ),
+        ),
+    ]
 
 # edx-drf-extensions csrf app
 urlpatterns += [
