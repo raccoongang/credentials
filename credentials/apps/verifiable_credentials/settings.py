11fcd3e826008f506a6b78bda436a615ac6c8846
Kyrylo Kholodenko <kyrylo.kholodenko@raccoongang.com>
2023-02-02 11:38:35 +0200

    feat: [OeX_Cred-224,OeX_Cred-363] add 'VC_DEFAULT_STORAGE' and 'VC_DEFAULT_ISSUANCE_URL' to settings

--- credentials/apps/verifiable_credentials/settings.py
+++ credentials/apps/verifiable_credentials/settings.py
@@ -4,7 +4,10 @@ The settings contains a boolean, ENABLE_VERIFIABLE_CREDENTIALS, indicating
 whether this app is enabled. Settings probes the ENABLE_VERIFIABLE_CREDENTIALS.
 If true, it calls apply_settings(), passing in the Django settings
 """
+from django.urls import reverse
 
 
 def apply_settings(django_settings):
     django_settings.VC_DEFAULT_STANDARD = "OBv3"
+    django_settings.VC_DEFAULT_STORAGE = "verifiable_credentials.storages.LCWalletStorage"
+    django_settings.VC_DEFAULT_ISSUANCE_URL = django_settings.ROOT_URL + reverse("verifiable_credentials:api:v1:wallet")
