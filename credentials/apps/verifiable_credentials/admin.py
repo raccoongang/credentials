fa7d30496b89cf37521686362e851ce229e4cab8
Kyrylo Kholodenko <kyrylo.kholodenko@raccoongang.com>
2023-02-13 10:31:53 +0200

    feat: [OeX_Cred-327, OeX_Cred-339] add CredentialIssuer and composing

--- credentials/apps/verifiable_credentials/admin.py
+++ credentials/apps/verifiable_credentials/admin.py
@@ -1,7 +1,7 @@
 from config_models.admin import KeyedConfigurationModelAdmin
 from django.contrib import admin
 
-from .models import IssuanceConfiguration
+from .models import IssuanceConfiguration, VerifiableCredentialIssuance
 from .toggles import is_verifiable_credentials_enabled
 
 
@@ -20,5 +20,12 @@ class IssuanceConfigurationAdmin(KeyedConfigurationModelAdmin):
         )
 
 
+class VerifiableCredentialIssuanceAdmin(admin.ModelAdmin):
+    list_display = ("uuid", "user_credential", "issuer_did", "processed")
+    list_filter = ("processed",)
+    search_fields = ("uuid",)
+
+
 if is_verifiable_credentials_enabled():
     admin.site.register(IssuanceConfiguration, IssuanceConfigurationAdmin)
+    admin.site.register(VerifiableCredentialIssuance, VerifiableCredentialIssuanceAdmin)
--- credentials/apps/verifiable_credentials/models.py
+++ credentials/apps/verifiable_credentials/models.py
@@ -1,13 +1,17 @@
 """
 Database models for verifiable_credentials.
 """
+import uuid
+
 from config_models.models import ConfigurationModel
 from django.conf import settings
 from django.contrib.sites.models import Site
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from django_extensions.db.models import TimeStampedModel
 
 from credentials.apps.catalog.models import Organization
+from credentials.apps.credentials.models import UserCredential
 
 from .constants import OPEN_BADGES_V3_KEY, VERIFIABLE_CREDENTIAL_KEY
 
@@ -55,3 +59,13 @@ class IssuanceConfiguration(ConfigurationModel):
         default=VERIFIABLE_CREDENTIAL_KEY,
         choices=DIGITAL_CREDENTIAL_FORMAT_CHOICES,
     )
+
+
+class VerifiableCredentialIssuance(TimeStampedModel):
+    """Data model representing a request for verifiable credentials"""
+
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    user_credential = models.ForeignKey(UserCredential, related_name="vc_issues", on_delete=models.CASCADE)
+    issuer_did = models.TextField()
+
+    processed = models.BooleanField(default=False)
