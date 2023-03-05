0a6aab0672d6cd9cc6e111a7c2a9ead8eef0ea99
Kyrylo Kholodenko <kyrylo.kholodenko@raccoongang.com>
2023-03-05 13:20:21 +0200

    feat: [OeX_Cred-603] compose open badges data model

--- credentials/apps/verifiable_credentials/composition/open_badges.py
+++ credentials/apps/verifiable_credentials/composition/open_badges.py
@@ -3,7 +3,54 @@ Open Badges 3.0 data model.
 
 See specification: https://1edtech.github.io/openbadges-specification/ob_v3p0.html
 """
+from enum import Enum
+
+from rest_framework import serializers
+
 from ..composition import BaseDataModel
+
+
+class Types(Enum):
+    VERIFIABLE_CREDENTIAL = "VerifiableCredential"
+    OPEN_BADGE_CREDENTIAL = "OpenBadgeCredential"
+    ISSUER_PROFILE = "IssuerProfile"
+    ACHIEVEMENT_SUBJECT = "AchievementSubject"
+    ACHIEVEMENT = "Achievement"
+
+
+class IssuerDataModel(serializers.Serializer):
+    id = serializers.CharField(source="issuer_id", read_only=True)
+    type = serializers.SerializerMethodField()
+    name = serializers.SerializerMethodField()
+
+    def get_type(self, *args, **kwargs):
+        return [Types.ISSUER_PROFILE.value]
+
+    def get_name(self, *args, **kwargs):
+        return "Example University"
+
+
+class AchievementDataModel(serializers.Serializer):
+    id = serializers.SerializerMethodField()
+    type = serializers.SerializerMethodField()
+
+    def get_type(self, *args, **kwargs):
+        return [Types.ACHIEVEMENT.value]
+
+    def get_id(self, issuance_line):
+        return issuance_line.user_credential.download_url
+
+
+class SubjectDataModel(serializers.Serializer):
+    id = serializers.CharField(source="subject_id", read_only=True)
+    type = serializers.SerializerMethodField()
+    achievement = serializers.SerializerMethodField()
+
+    def get_achievement(self, issuance_line):
+        return AchievementDataModel(issuance_line).data
+
+    def get_type(self, *args, **kwargs):
+        return [Types.ACHIEVEMENT_SUBJECT.value]
 
 
 class OpenBadgesDataModel(BaseDataModel):
@@ -13,33 +60,32 @@ class OpenBadgesDataModel(BaseDataModel):
 
     VERSION = 3.0
 
-    def to_representation(self, instance):
-        return {
-            "@context": [
-                "https://www.w3.org/2018/credentials/v1",
-                "https://www.w3.org/2018/credentials/examples/v1",
-                "https://purl.imsglobal.org/spec/ob/v3p0/context.json",
-            ],
-            "id": "http://example.edu/credentials/3732",
-            "type": ["VerifiableCredential", "OpenBadgeCredential"],
-            "issuer": {
-                "id": "https://example.edu/issuers/565049",
-                "type": ["IssuerProfile"],
-                "name": "Example University",
-            },
-            "issuanceDate": "2010-01-01T00:00:00Z",
-            "name": "Teamwork Badge",
-            "credentialSubject": {
-                "id": "did:example:ebfeb1f712ebc6f1c276e12ec21",
-                "type": ["AchievementSubject"],
-                "achievement": {
-                    "id": "https://example.com/achievements/21st-century-skills/teamwork",
-                    "type": ["Achievement"],
-                    "criteria": {
-                        "narrative": "Team members are nominated for this badge by their peers and recognized upon review by Example Corp management."
-                    },
-                    "description": "This badge recognizes the development of the capacity to collaborate within a group environment.",
-                    "name": "Teamwork",
-                },
-            },
-        }
+    type = serializers.SerializerMethodField()
+    issuer = serializers.SerializerMethodField()
+    issuanceDate = serializers.DateTimeField(source="modified", read_only=True)
+    name = serializers.SerializerMethodField()
+    credentialSubject = serializers.SerializerMethodField(method_name="get_subject")
+
+    def get_subject(self, issuance_line):
+        return SubjectDataModel(issuance_line).data
+
+    def get_name(self, issuance_line):
+        return issuance_line.user_credential.credential_content_type.model
+
+    def get_issuer(self, issuance_line):
+        return IssuerDataModel(issuance_line).data
+
+    def get_type(self, *args, **kwargs):
+        default_types = [
+            Types.VERIFIABLE_CREDENTIAL.value,
+            Types.OPEN_BADGE_CREDENTIAL.value,
+        ]
+        return default_types
+
+    @property
+    def context(self):
+        return [
+            "https://www.w3.org/2018/credentials/v1",
+            "https://www.w3.org/2018/credentials/examples/v1",
+            "https://purl.imsglobal.org/spec/ob/v3p0/context.json",
+        ]
