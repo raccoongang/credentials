b11ef0ddffa9754ebcc4053f3071e222bd358fc4
Kyrylo Kholodenko <kyrylo.kholodenko@gmail.com>
2024-01-17 16:36:09 +0200

    feat: [ACI-222] feature flag for badges (#52)

--- credentials/apps/badges/apps.py
+++ credentials/apps/badges/apps.py
@@ -1,16 +1,16 @@
 from django.apps import AppConfig
 
-from .toggles import is_badges_enabled
+from .toggles import check_badges_enabled
 
 
 class BadgesConfig(AppConfig):
     name = "credentials.apps.badges"
     verbose_name = "Badges"
 
+    @check_badges_enabled
     def ready(self):
         """
         Performs initial registrations for checks, signals, etc.
         """
-        if is_badges_enabled():
-            from . import signals  # pylint: disable=unused-import,import-outside-toplevel
-            from . checks import badges_checks  # pylint: disable=unused-import,import-outside-toplevel
+        from . import signals  # pylint: disable=unused-import,import-outside-toplevel
+        from . checks import badges_checks  # pylint: disable=unused-import,import-outside-toplevel
