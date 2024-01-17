39bc6e7ab50c0f4639a6584a36cad8102b306353
Kyrylo Kholodenko <kyrylo.kholodenko@gmail.com>
2024-01-17 16:36:09 +0200

    feat: [ACI-222] feature flag for badges (#52)

--- credentials/apps/badges/admin.py
+++ credentials/apps/badges/admin.py
@@ -1,3 +1,9 @@
 """
 Admin section configuration.
 """
+
+from .toggles import is_badges_enabled
+
+if is_badges_enabled():
+    # TODO: Define registering admin classes here `admin.site.register(...)`
+    pass
