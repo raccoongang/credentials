e0c407f37544785f1ecec1d86e9fe50fc26b6948
Kyrylo Kholodenko <kyrylo.kholodenko@raccoongang.com>
2024-01-18 17:16:43 +0200

    feat: [ACI-294] implement credly api client

--- credentials/apps/badges/distribution/credly/credly_badges/settings/base.py
+++ credentials/apps/badges/distribution/credly/credly_badges/settings/base.py
@@ -3,4 +3,4 @@ Credly Badges base settings.
 """
 
 def plugin_settings(settings):  # pylint: disable=unused-argument
-    pass
+    settings.CREDLY_API_BASE_URL = 'https://sandbox-api.credly.com/v1/'
