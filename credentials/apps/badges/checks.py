1889e56163d29c0b7dc7666874c2a2ec5e32f529
Kyrylo Kholodenko <kyrylo.kholodenko@raccoongang.com>
2024-01-23 00:53:57 +0200

    feat: [ACI-228, ACI-234] badges configurations

--- credentials/apps/badges/checks.py
+++ credentials/apps/badges/checks.py
@@ -1,12 +1,30 @@
 """
 Badges checks.
 """
+from django.conf import settings
 from django.core.checks import Error, Tags, register
 
 
 @register(Tags.compatibility)
 def badges_checks(*args, **kwargs):
     """
+    Checks the consistency of the badges configurations.
+
+    Raises compatibility Errors upon:
+        - BADGES_CONFIG['events'] is empty
+
+    Returns:
+        List of any Errors.
     """
     errors = []
+
+    if not settings.BADGES_CONFIG.get("events"):
+        errors.append(
+            Error(
+                "BADGES_CONFIG['events'] must include at least one event.",
+                hint="Add at least one event to BADGES_CONFIG['events'] setting.",
+                id="badges.E001",
+            )
+        )
+
     return errors
