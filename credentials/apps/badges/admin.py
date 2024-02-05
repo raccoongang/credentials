e6e3f32efc0a6cf58555b8352cd069a0bceed83d
Kyrylo Kholodenko <kyrylo.kholodenko@raccoongang.com>
2024-02-05 19:28:52 +0200

    refactor: [ACI-387] badge template to base model

--- credentials/apps/badges/admin.py
+++ credentials/apps/badges/admin.py
@@ -4,8 +4,18 @@ Admin section configuration.
 from django.contrib import admin
 
 from .toggles import is_badges_enabled
+from .models import BadgeTemplate
+
+
+class BadgeTemplateAdmin(admin.ModelAdmin):
+    """
+    Badge template admin setup.
+    """
+    list_display = ("name", "uuid", "status", "type", "is_active",)
+    list_filter = ("status", "type", "is_active",)
+    search_fields = ("name", "uuid",)
 
 
 # register admin configurations with respect to the feature flag
 if is_badges_enabled():
-    pass
+    admin.site.register(BadgeTemplate, BadgeTemplateAdmin)
--- credentials/apps/badges/models.py
+++ credentials/apps/badges/models.py
@@ -1,3 +1,36 @@
 """
 Badges DB models.
 """
+
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+from credentials.apps.core.utils import _choices
+from credentials.apps.credentials.models import AbstractCredential
+
+from .constants import BadgeTemplateStatus
+
+
+class BadgeTemplate(AbstractCredential):
+    """
+    Describes badge credential type.
+    """
+
+    uuid = models.UUIDField(unique=True, help_text=_("Unique badge template ID."))
+    name = models.CharField(max_length=255, help_text=_("Badge template name."))
+    type = models.CharField(max_length=255, default="openedx")
+    status = models.CharField(
+        max_length=255,
+        choices=_choices(
+            BadgeTemplateStatus.ACTIVE,
+            BadgeTemplateStatus.ARCHIVED,
+            BadgeTemplateStatus.DRAFT,
+            BadgeTemplateStatus.REVOKED,
+        ),
+        help_text=_("Status of the badge template."),
+    )
+    description = models.TextField(null=True, blank=True, help_text=_("Badge template description."))
+    icon = models.ImageField(upload_to="badge_templates/icons", null=True, blank=True)
+
+    def __str__(self):
+        return self.name
