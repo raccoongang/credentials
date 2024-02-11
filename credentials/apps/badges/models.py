500228f313d8ac925f5fcafdd8bd41c50af9d7e5
wowkalucky <wowkalucky@gmail.com>
2024-02-11 18:25:02 +0200

    quality: auto-format

--- credentials/apps/badges/models.py
+++ credentials/apps/badges/models.py
@@ -17,17 +17,11 @@ class BadgeTemplate(AbstractCredential):
 
     TYPE = "openedx"
 
-    uuid = models.UUIDField(
-        unique=True, default=uuid.uuid4, help_text=_("Unique badge template ID.")
-    )
+    uuid = models.UUIDField(unique=True, default=uuid.uuid4, help_text=_("Unique badge template ID."))
     name = models.CharField(max_length=255, help_text=_("Badge template name."))
-    description = models.TextField(
-        null=True, blank=True, help_text=_("Badge template description.")
-    )
+    description = models.TextField(null=True, blank=True, help_text=_("Badge template description."))
     icon = models.ImageField(upload_to="badge_templates/icons", null=True, blank=True)
-    origin = models.CharField(
-        max_length=128, null=True, blank=True, help_text=_("Badge template type.")
-    )
+    origin = models.CharField(max_length=128, null=True, blank=True, help_text=_("Badge template type."))
 
     def __str__(self):
         return self.name
