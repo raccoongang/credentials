d62f4c820f1e17893e6a9c9505ace10e62c0e616
Andrii <andrii.hantkovskyi@raccoongang.com>
2024-03-26 14:13:24 +0200

    feat: [ACI-741] fill badge_data for event

--- credentials/apps/badges/signals/handlers.py
+++ credentials/apps/badges/signals/handlers.py
@@ -11,6 +11,8 @@ from openedx_events.learning.data import BadgeData, BadgeTemplateData
 from openedx_events.learning.signals import BADGE_REVOKED
 from openedx_events.tooling import OpenEdxPublicSignal, load_all_signals
 
+from apps.core.api import get_user_by_username
+
 from .signals import BADGE_PROGRESS_INCOMPLETE
 from ..services.badge_templates import get_badge_template_by_id
 from ..utils import get_badging_event_types
@@ -45,12 +47,13 @@ def event_handler(sender, signal, **kwargs):
 @receiver(BADGE_PROGRESS_INCOMPLETE)
 def listen_for_incompleted_badge(sender, username, badge_template_id, **kwargs):  # pylint: disable=unused-argument
     badge_template = get_badge_template_by_id(badge_template_id)
-    ...
+    user = get_user_by_username(username)
 
-    # `uuid` and `user` empty until user retrieving from LMS is implemented
+    badge = revoke_badge() # plug until function is implemented
+
     badge_data = BadgeData(
-        uuid=...,
-        user=...,
+        uuid=badge.uuid,
+        user=user,
         template=BadgeTemplateData(
             uuid=str(badge_template.uuid),
             type=badge_template.origin,
