9d5ccc2c8b8962e64c34b0f1a7919cb5c1baf75d
andrii-hantkovskyi <131773947+andrii-hantkovskyi@users.noreply.github.com>
2024-04-03 20:15:09 +0300

    feat: [ACI-776] add tests to ensure posibility of multiple requirements (#98)
    Co-authored-by: Andrii <andrii.hantkovskyi@raccoongang.com>

--- credentials/apps/badges/tests/test_models.py
+++ credentials/apps/badges/tests/test_models.py
@@ -3,10 +3,10 @@ import uuid
 from django.contrib.sites.models import Site
 from django.test import TestCase
 
-from ..models import BadgeRequirement, CredlyBadgeTemplate, CredlyOrganization, DataRule
+from ..models import BadgeRequirement, BadgeTemplate, CredlyBadgeTemplate, CredlyOrganization, DataRule
 
 
-class BadgeRequirementTestCase(TestCase):
+class DataRulesTestCase(TestCase):
     def setUp(self):
         self.organization = CredlyOrganization.objects.create(uuid=uuid.uuid4(), api_key="test-api-key", name="test_organization")
         self.site = Site.objects.create(domain="test_domain", name="test_name")
@@ -37,3 +37,65 @@ class BadgeRequirementTestCase(TestCase):
         self.assertEqual(data_rules.count(), 2)
         self.assertIn(data_rule1, data_rules)
         self.assertIn(data_rule2, data_rules)
+
+
+class BadgeRequirementTestCase(TestCase):
+    def setUp(self):
+        self.organization = CredlyOrganization.objects.create(uuid=uuid.uuid4(), api_key="test-api-key", name="test_organization")
+        self.site = Site.objects.create(domain="test_domain", name="test_name")
+        self.badge_template = BadgeTemplate.objects.create(uuid=uuid.uuid4(), name="test_template", state="draft", site=self.site)
+        self.credlybadge_template = CredlyBadgeTemplate.objects.create(organization=self.organization, uuid=uuid.uuid4(), name="test_template", state="draft", site=self.site)
+    
+    def test_multiple_requirements_for_badgetemplate(self):
+        self.requirement1 = BadgeRequirement.objects.create(
+            template=self.badge_template,
+            event_type="org.openedx.learning.course.passing.status.updated.v1",
+            effect="award",
+            description="Test description",
+        )
+        self.requirement2 = BadgeRequirement.objects.create(
+            template=self.badge_template,
+            event_type="org.openedx.learning.course.passing.status.updated.v1",
+            effect="revoke",
+            description="Test description",
+        )
+        self.requirement3 = BadgeRequirement.objects.create(
+            template=self.badge_template,
+            event_type="org.openedx.learning.ccx.course.passing.status.updated.v1",
+            effect="award",
+            description="Test description",
+        )
+
+        requirements = BadgeRequirement.objects.filter(template=self.badge_template)
+
+        self.assertEqual(requirements.count(), 3)
+        self.assertIn(self.requirement1, requirements)
+        self.assertIn(self.requirement2, requirements)
+        self.assertIn(self.requirement3, requirements)
+
+    def test_multiple_requirements_for_credlybadgetemplate(self):
+        self.requirement1 = BadgeRequirement.objects.create(
+            template=self.credlybadge_template,
+            event_type="org.openedx.learning.ccx.course.passing.status.updated.v1",
+            effect="award",
+            description="Test description",
+        )
+        self.requirement2 = BadgeRequirement.objects.create(
+            template=self.credlybadge_template,
+            event_type="org.openedx.learning.ccx.course.passing.status.updated.v1",
+            effect="revoke",
+            description="Test description",
+        )
+        self.requirement3 = BadgeRequirement.objects.create(
+            template=self.credlybadge_template,
+            event_type="org.openedx.learning.course.passing.status.updated.v1",
+            effect="award",
+            description="Test description",
+        )
+
+        requirements = BadgeRequirement.objects.filter(template=self.credlybadge_template)
+
+        self.assertEqual(requirements.count(), 3)
+        self.assertIn(self.requirement1, requirements)
+        self.assertIn(self.requirement2, requirements)
+        self.assertIn(self.requirement3, requirements)
