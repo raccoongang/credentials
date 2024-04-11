8670fbe1048aba304a1d767040f39911ed54d344
Andrii <andrii.hantkovskyi@raccoongang.com>
2024-04-11 18:24:40 +0300

    feat: [ACI-917] forbid not configured template activation

--- credentials/apps/badges/models.py
+++ credentials/apps/badges/models.py
@@ -102,11 +102,14 @@ class BadgeTemplate(AbstractCredential):
         return self.name
 
     def save(self, *args, **kwargs):
+        if self.is_active and self.badgerequirement_set.count() == 0:
+            raise ValidationError("Badge template must have at least 1 Requirement set.")
         super().save()
         # auto-evaluate type:
         if not self.origin:
             self.origin = self.ORIGIN
             self.save(*args, **kwargs)
+
 
     @classmethod
     def by_uuid(cls, template_uuid):
