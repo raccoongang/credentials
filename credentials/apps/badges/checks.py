a7723a12bda4b16ca3283d0f5b1538342c7fd87a
Andrii <andrii.hantkovskyi@raccoongang.com>
2024-04-16 16:15:07 +0300

    feat: [ACI-928] add badgesconfig consistency check

--- credentials/apps/badges/checks.py
+++ credentials/apps/badges/checks.py
@@ -4,7 +4,7 @@ Badges app self-checks.
 
 from django.core.checks import Error, Tags, register
 
-from .utils import get_badging_event_types
+from .utils import credly_check, get_badging_event_types
 
 
 @register(Tags.compatibility)
@@ -26,6 +26,14 @@ def badges_checks(*args, **kwargs):
                 "BADGES_CONFIG['events'] must include at least one event.",
                 hint="Add at least one event to BADGES_CONFIG['events'] setting.",
                 id="badges.E001",
+            )
+        )
+    if not credly_check():
+        errors.append(
+            Error(
+                "Credly settings are not properly configured.",
+                hint="Make sure all required settings are present in BADGES_CONFIG['credly'].",
+                id="badges.E002",
             )
         )
 
