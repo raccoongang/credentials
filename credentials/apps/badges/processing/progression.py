2261e04d48603d8d2617e81e7f30623cbab74d63
Andrii <andrii.hantkovskyi@raccoongang.com>
2024-04-16 15:30:34 +0300

    feat: [ACI-930 make discover requirement collect only active]

--- credentials/apps/badges/processing/progression.py
+++ credentials/apps/badges/processing/progression.py
@@ -15,7 +15,7 @@ def discover_requirements(event_type: str) -> List[BadgeRequirement]:
     """
     Picks all relevant requirements based on the event type.
     """
-    return BadgeRequirement.objects.filter(event_type=event_type)
+    return BadgeRequirement.objects.filter(event_type=event_type, template__is_active=True)
 
 
 def process_requirements(event_type, username, payload_dict):
@@ -29,11 +29,6 @@ def process_requirements(event_type, username, payload_dict):
     logger.debug("BADGES: found %s requirements to process.", len(requirements))
 
     for requirement in requirements:
-
-        # ignore: if the badge template wasn't activated yet
-        if not requirement.is_active:
-            continue
-
         # remember: the badge template is already "done"
         if requirement.template.is_completed(username):
             completed_templates.add(requirement.template_id)
