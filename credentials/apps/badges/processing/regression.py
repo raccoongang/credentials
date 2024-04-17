ed840d6f2826efb93208900030684b4c38b5f792
andrii-hantkovskyi <131773947+andrii-hantkovskyi@users.noreply.github.com>
2024-04-17 17:52:42 +0300

    feat: [ACI-931] make discover penalties collect only active (#144)
    Co-authored-by: Andrii <andrii.hantkovskyi@raccoongang.com>
    Co-authored-by: Volodymyr Bergman <volodymyr.bergman@raccoongang.com>

--- credentials/apps/badges/processing/regression.py
+++ credentials/apps/badges/processing/regression.py
@@ -15,9 +15,8 @@ def discover_penalties(event_type: str) -> List[BadgePenalty]:
     """
     Picks all relevant penalties based on the event type.
     """
-
-    # TODO: get only active templates
-    return BadgePenalty.objects.filter(event_type=event_type)
+ 
+    return BadgePenalty.objects.filter(event_type=event_type, template__is_active=True)
 
 
 def process_penalties(event_type, username, payload_dict):
@@ -30,10 +29,6 @@ def process_penalties(event_type, username, payload_dict):
     logger.debug("BADGES: found %s penalties to process.", len(penalties))
 
     for penalty in penalties:
-
-        # TODO: remove the check if only active templates were collected
-        if not penalty.is_active:
-            continue
 
         # process: payload rules
         if penalty.apply_rules(payload_dict):
