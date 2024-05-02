f3e72e5bec611cdac6f7639c5e0498db4dd0f4fc
Andrii <andrii.hantkovskyi@raccoongang.com>
2024-05-02 11:58:04 +0300

    refactor: [ACI-963] reduce git diff in

--- credentials/settings/base.py
+++ credentials/settings/base.py
@@ -378,11 +378,7 @@ CREDENTIALS_SERVICE_USER = "credentials_service_user"
 
 JWT_AUTH = {
     "JWT_ISSUER": [
-        {
-            "AUDIENCE": "SET-ME-PLEASE",
-            "ISSUER": "http://127.0.0.1:8000/oauth2",
-            "SECRET_KEY": "SET-ME-PLEASE",
-        }
+        {"AUDIENCE": "SET-ME-PLEASE", "ISSUER": "http://127.0.0.1:8000/oauth2", "SECRET_KEY": "SET-ME-PLEASE"}
     ],
     "JWT_ALGORITHM": "HS256",
     "JWT_VERIFY_EXPIRATION": True,
@@ -401,9 +397,7 @@ EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 ACE_ENABLED_CHANNELS = ["django_email"]
 ACE_CHANNEL_DEFAULT_EMAIL = "django_email"
 ACE_CHANNEL_TRANSACTIONAL_EMAIL = "django_email"
-ACE_CHANNEL_SAILTHRU_TEMPLATE_NAME = (
-    ""  # unused, but required to be set or we see an exception
-)
+ACE_CHANNEL_SAILTHRU_TEMPLATE_NAME = ""  # unused, but required to be set or we see an exception
 
 # Set up logging for development use (logging to stdout)
 LOGGING = get_logger_config(debug=DEBUG, dev_env=True, local_loglevel="DEBUG")
@@ -416,9 +410,7 @@ REST_FRAMEWORK = {
     ),
     "DEFAULT_FILTER_BACKENDS": ("django_filters.rest_framework.DjangoFilterBackend",),
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "DEFAULT_PERMISSION_CLASSES": (
-        "rest_framework.permissions.DjangoModelPermissions",
-    ),
+    "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.DjangoModelPermissions",),
     "PAGE_SIZE": 20,
     "DATETIME_FORMAT": "%Y-%m-%dT%H:%M:%SZ",
     "EXCEPTION_HANDLER": "credentials.apps.api.v2.views.credentials_throttle_handler",
@@ -537,9 +529,7 @@ ALLOWED_EMAIL_HTML_TAGS = [
 # .. toggle_creation_date: 2021-04-30
 # .. toggle_target_removal_date: 2021-06-01
 # .. toggle_tickets: https://openedx.atlassian.net/browse/MICROBA-1169, https://openedx.atlassian.net/browse/MICROBA-1198 (ticket for removal)
-USE_CERTIFICATE_AVAILABLE_DATE = WaffleSwitch(
-    "credentials.use_certificate_available_date", module_name=__name__
-)
+USE_CERTIFICATE_AVAILABLE_DATE = WaffleSwitch("credentials.use_certificate_available_date", module_name=__name__)
 
 LOGO_TRADEMARK_URL = "https://edx-cdn.org/v3/default/logo-trademark.svg"
 LOGO_TRADEMARK_URL_PNG = "https://edx-cdn.org/v3/default/logo-trademark.png"
@@ -573,10 +563,7 @@ EVENT_BUS_PRODUCER_CONFIG = {
     # .. toggle_warning: The default may be changed in a later release.
     # .. toggle_use_cases: opt_in
     "org.openedx.learning.badge.awarded.v1": {
-        "learning-badges-lifecycle": {
-            "event_key_field": "badge.uuid",
-            "enabled": BADGES_ENABLED,
-        },
+        "learning-badges-lifecycle": {"event_key_field": "badge.uuid", "enabled": BADGES_ENABLED},
     },
     # .. setting_name: EVENT_BUS_PRODUCER_CONFIG['org.openedx.learning.badge.revoked.v1']
     #    ['learning-badges-lifecycle']['enabled']
@@ -586,10 +573,7 @@ EVENT_BUS_PRODUCER_CONFIG = {
     # .. toggle_warning: The default may be changed in a later release.
     # .. toggle_use_cases: opt_in
     "org.openedx.learning.badge.revoked.v1": {
-        "learning-badges-lifecycle": {
-            "event_key_field": "badge.uuid",
-            "enabled": BADGES_ENABLED,
-        },
+        "learning-badges-lifecycle": {"event_key_field": "badge.uuid", "enabled": BADGES_ENABLED},
     },
 }
 
